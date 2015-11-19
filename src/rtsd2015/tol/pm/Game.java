821197940cae9912d40fa07fe6ae06f243ed4164
Ari H <ari.hoysniemi@gmail.com>
2015-11-19 19:22:52 +0200

    gameloop's first parts

--- src/rtsd2015/tol/pm/Game.java
+++ src/rtsd2015/tol/pm/Game.java
@@ -8,17 +8,30 @@ public class Game {
 	private Level lvl;
 	private ArrayList<Player> players;
 	private ArrayList<Score> scores;
-	private boolean ingame;
 
-	public Game() {
+	private static int limitedCycles = 1280;	// For testing purposes only
+	private static int fps = 0;
+	private static long lastFpsTime;
+	private static long frameId;
+
+	/**
+	 * Starts a new game locally
+	 *
+	 * @throws InterruptedException
+	 */
+	public Game() throws InterruptedException {
 		boolean initialized = initGame();
 		if (initialized) {
-			ingame = true;
-			gameCycle();
+			gameLoop();
 		}
 		// TODO: end game
 	}
 
+	/**
+	 * Initializes the new game
+	 *
+	 * @return true if everything was OK and game can begin
+	 */
 	private boolean initGame() {
 		// TODO: init game elements
 		timer = new Timer();
@@ -26,10 +39,37 @@ public class Game {
 		return false;
 	}
 
-	private void gameCycle() {
-		long cycle = 0;
-		while (ingame && (cycle < 100)) {
-			cycle++;
+	/**
+	 * The main gameloop
+	 *
+	 * @throws InterruptedException
+	 */
+	private static void gameLoop() throws InterruptedException {
+
+		// Init game loop
+		long		lastLoopTime	= System.nanoTime();
+		final int	TARGET_FPS		= 16;
+		final long	OPTIMAL_TIME	= 1000000000 / TARGET_FPS;
+		// TODO: GUI-call
+
+		while (limitedCycles > 0) {
+
+			// Define this cycle
+			long now 				= System.nanoTime();
+			long updateLength 		= now - lastLoopTime;
+			lastLoopTime 			= now;
+			double delta 			= updateLength / ((double)OPTIMAL_TIME);
+
+			// Update counters
+			lastFpsTime 			+= updateLength;
+			fps++;
+			frameId++;
+			limitedCycles--;
+
+			// TODO: content of the cycle
+
+			// Wait for the next frame
+			Thread.sleep((lastLoopTime-System.nanoTime()+OPTIMAL_TIME)/1000000);
 		}
 	}
 
--- src/rtsd2015/tol/pm/JoinGame.java
+++ src/rtsd2015/tol/pm/JoinGame.java
@@ -1,7 +1,7 @@
 package rtsd2015.tol.pm;
 
 public class JoinGame {
-	public JoinGame(String[] args) {
+	public JoinGame(String[] args) throws InterruptedException {
 		Game game = new Game();
 	}
 }
