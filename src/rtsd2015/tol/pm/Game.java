5a5b9cab44da9b59713ccf8f91c18a8e1d589adc
Ari Höysniemi <ari.hoysniemi@gmail.com>
2015-11-21 19:39:47 +0200

    Usage of enums. A lot of placeholder thingys.

--- src/rtsd2015/tol/pm/Game.java
+++ src/rtsd2015/tol/pm/Game.java
@@ -3,25 +3,33 @@ package rtsd2015.tol.pm;
 import java.util.ArrayList;
 import java.util.List;
 
+import rtsd2015.tol.pm.enums.Side;
+
 public class Game {
 
-	private Timer timer;
-	private Level lvl;
-	private List<Player> players;
-	private List<Score> scores;
+	private static Timer timer;
+	private static Level lvl;
+	private static List<Player> players = new ArrayList<>();
 
-	private static int limitedCycles = 1280;	// For testing purposes only
+	private static int limitedCycles = 1280; // For testing purposes only
 	private static long lastFpsTime;
 	private static long frameId;
-	
+
 	/**
 	 * Initializes a new game
 	 * 
 	 * @throws InterruptedException
 	 */
 	Game() throws InterruptedException {
+		
+		// Initialize players
+		players.add(new Player(Side.BLUE));
+		players.add(new Player(Side.RED));
+		
+		// Initialize in-game timer
 		timer = new Timer();
-		timer.start();
+
+		// Finally, start the game loop
 		gameLoop();
 	}
 
@@ -32,31 +40,35 @@ public class Game {
 	 */
 	private static void gameLoop() throws InterruptedException {
 
-		// Init game loop
-		long		lastLoopTime	= System.nanoTime();
-		final int	TARGET_FPS		= 16;
-		final long	OPTIMAL_TIME	= 1000000000 / TARGET_FPS;
+		// Initialize game loop
+		long lastLoopTime = System.nanoTime();
+		final int TARGET_FPS = 16;
+		final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+
+		// Start timer
+		timer.start();
 
 		while (limitedCycles > 0) {
 
 			// Define this cycle
-			long now 				= System.nanoTime();
-			long updateLength 		= now - lastLoopTime;
-			lastLoopTime 			= now;
-			double delta 			= updateLength / ((double)OPTIMAL_TIME);
+			long now = System.nanoTime();
+			long updateLength = now - lastLoopTime;
+			lastLoopTime = now;
+			double delta = updateLength / ((double) OPTIMAL_TIME);
 
 			// Update counters
-			lastFpsTime 			+= updateLength;
+			lastFpsTime += updateLength;
 			frameId++;
 			limitedCycles--;
 
 			// TODO: content of the cycle
 			System.out.println("Current frame: " + frameId + ", frames left: " + limitedCycles);
+			System.out.println("Player 1 side: " + players.get(0).getSide() + " dir: " + players.get(0).getDir());
+			System.out.println("Player 2 side: " + players.get(1).getSide() + " dir: " + players.get(1).getDir());
 
 			// Wait for the next frame
-			Thread.sleep((lastLoopTime-System.nanoTime()+OPTIMAL_TIME)/1000000);
+			Thread.sleep((lastLoopTime - System.nanoTime() + OPTIMAL_TIME) / 1000000);
 		}
 	}
-
 
 }
--- src/rtsd2015/tol/pm/Player.java
+++ src/rtsd2015/tol/pm/Player.java
@@ -1,10 +1,88 @@
 package rtsd2015.tol.pm;
 
+import java.util.ArrayList;
+
+import rtsd2015.tol.pm.enums.Facing;
+import rtsd2015.tol.pm.enums.Side;
+
+/**
+ * The actual player entity
+ * 
+ * @author Ari
+ */
 public class Player {
-	public int posX;
-	public int posY;
-	public int health;
-	public int speed;
-	public int dir;
-	// draw
+	
+	public static int playerCount;
+	
+	private int health;
+	private int speed;
+	private int score;
+	private Side side;
+	private Facing dir;
+	private int[] position;
+
+	/**
+	 * Initialize a new player
+	 * 
+	 * @param Side
+	 */
+	Player(Side s) {
+		side = s;
+		switch (side) {
+		case BLUE:
+			setDir(Facing.EAST);
+			break;
+		case RED:
+			setDir(Facing.WEST);
+			break;
+		}
+		playerCount++;
+	}
+
+	/**
+	 * Set player position
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	void setPos(int x, int y) {
+		position[0] = x;
+		position[1] = y;
+	}
+
+	/**
+	 * Get player position
+	 * 
+	 * @return (x,y)
+	 */
+	int[] getPos() {
+		return position;
+	}
+
+	/**
+	 * Return player's side
+	 * 
+	 * @return Side
+	 */
+	Side getSide() {
+		return side;
+	}
+
+	/**
+	 * Set a new direction for the player
+	 * 
+	 * @param newDir
+	 */
+	void setDir(Facing d) {
+		dir = d;
+	}
+
+	/**
+	 * Return player's direction
+	 * 
+	 * @return Facing
+	 */
+	Facing getDir() {
+		return dir;
+	}
 }
