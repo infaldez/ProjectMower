61a74b91f834c94fb07fc20b375cb62976dd4e10
Janne Sänkiaho <sankiahojanne@gmail.com>
2016-01-06 14:26:25 +0200

    change dir

--- src/rtsd2015/tol/pm/KeyboardInput.java
+++ src/rtsd2015/tol/pm/KeyboardInput.java
@@ -1,5 +1,7 @@
 package rtsd2015.tol.pm;
 
+import rtsd2015.tol.pm.enums.Facing;
+
 import javafx.event.EventHandler;
 import javafx.scene.*;
 import javafx.scene.input.KeyEvent;
@@ -15,15 +17,19 @@ public class KeyboardInput {
                 switch (event.getCode()) {
                     case UP:
                     	game.getPlayers().get(0).setPos(game.getPlayers().get(0).getPos()[0], game.getPlayers().get(0).getPos()[1] -1);
+                    	game.getPlayers().get(0).setDir(Facing.NORTH);
                     	break;
                     case DOWN:
                     	game.getPlayers().get(0).setPos(game.getPlayers().get(0).getPos()[0], game.getPlayers().get(0).getPos()[1] +1);
+                    	game.getPlayers().get(0).setDir(Facing.SOUTH);
 	                	break;
                     case LEFT:
                     	game.getPlayers().get(0).setPos(game.getPlayers().get(0).getPos()[0] -1, game.getPlayers().get(0).getPos()[1]);
+                    	game.getPlayers().get(0).setDir(Facing.WEST);
 	                	break;
                     case RIGHT:
 	                	game.getPlayers().get(0).setPos(game.getPlayers().get(0).getPos()[0] +1, game.getPlayers().get(0).getPos()[1]);
+	                	game.getPlayers().get(0).setDir(Facing.EAST);
 	                	break;
                 }
             }
