dc0be2638d926eb09583753a944ab5352b02bab6
Ari H <ari.hoysniemi@gmail.com>
2016-01-10 17:07:29 +0200

    Removed a warning

--- src/rtsd2015/tol/pm/view/NewHostDialogController.java
+++ src/rtsd2015/tol/pm/view/NewHostDialogController.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import rtsd2015.tol.pm.Launcher;
