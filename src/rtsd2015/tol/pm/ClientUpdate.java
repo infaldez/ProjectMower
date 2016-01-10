ab8b7a6927fff39ca0356276ccda90194d3087f6
Ari H <ari.hoysniemi@gmail.com>
2016-01-10 19:57:59 +0200

    Removed not required classes

--- src/rtsd2015/tol/pm/ClientUpdate.java
+++
@@ -1,8 +0,0 @@
-package rtsd2015.tol.pm;
-
-public class ClientUpdate {
-    @SuppressWarnings("unused")
-	private final int[] p1Pos = {1,2};
-    @SuppressWarnings("unused")
-	private final int[] p2Pos = {5,3};
-}
